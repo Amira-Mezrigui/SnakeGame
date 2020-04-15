c773a88e3b0b4b6d88369ac8d0a5f1f5881a7528
ahmed <ahmedgabsisimplon@gmail.com>
2020-04-15 23:28:30 +0100

    j'ai ajouté la fonction path et j'ai déclaré snake

--- snake.js
+++ snake.js
@@ -14,3 +14,34 @@ let ground = setInterval(draw,100);
 // the star image
 const starImg = new Image();
 starImg.src = "img/star.jpg";
+ 
+// create the snake
+
+let snake = [{
+    x : 12* unity,
+    y : 12 * unity
+}];
+
+
+// create the score var
+
+let score = 0;
+
+//control the snake
+
+let direction;
+
+document.addEventListener("keydown",direction);
+
+function path(event){
+    let key = event.which;
+    if( (key == 37 || key== 81) && direction != "RIGHT"){
+        direction = "LEFT";
+    }else if((key == 38 || key== 90) && direction != "DOWN"){
+        direction = "UP";
+    }else if((key == 39 || key== 68) && direction != "LEFT"){
+        direction= "RIGHT";
+    }else if((key == 40 || key== 83) && direction!=  "UP"){
+        direction= "DOWN";
+    }
+}
