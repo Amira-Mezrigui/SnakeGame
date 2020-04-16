174f63cd5231f01d63e939586e7651cacc03ed7e
khaled mehrez <khaledmehrezsimplon@gmail.com>
2020-04-16 12:33:34 +0100

    ajouter la fonction draw

--- snake.js
+++ snake.js
@@ -1,47 +1,110 @@
+/*add variables that i need*/
+//add the canvas
 var canvas = document.getElementById("snakeCanvas");
 var ctx = canvas.getContext("2d");
 
 // the unity of background
-const unity = 25;
+const unity = 20;
 
-//backgound image
-const groundSnake = new Image();
-groundSnake.src = "img/ground.jpeg";
-function draw(){
-ctx.drawImage(groundSnake,0,0);
-}
-let ground = setInterval(draw,100);
-// the star image
-const starImg = new Image();
-starImg.src = "img/star.jpg";
- 
+
 // create the snake
 
 let snake = [{
-    x : 12* unity,
-    y : 12 * unity
+    x :  5*unity,
+    y :  5*unity
 }];
+//generate the food
 
+console.log(star)
 
 // create the score var
 
 let score = 0;
 
-//control the snake
+/*function that allows me to know the directio of the snake*/
 
 let direction;
 
-document.addEventListener("keydown",direction);
+document.addEventListener("keydown",path);
 
 function path(event){
     let key = event.which;
-    if( (key == 37 || key== 81) && direction != "RIGHT"){
+    if (key == 37||key== 81 && direction!= "RIGHT"){
         direction = "LEFT";
-    }else if((key == 38 || key== 90) && direction != "DOWN"){
+    }else if(key == 38 || key== 90  && direction != "DOWN"){
         direction = "UP";
-    }else if((key == 39 || key== 68) && direction != "LEFT"){
+    }else if(key == 39 ||key== 68 && direction!= "LEFT"){
         direction= "RIGHT";
-    }else if((key == 40 || key== 83) && direction!=  "UP"){
+    }else if(key == 40 ||key== 83 && direction!=  "UP"){
         direction= "DOWN";
     }
 }
+     /*create the game*/
+function draw(){
+    /*draw elements of the game*/
+    //add the background
+
+     const groundSnake = new Image();
+     groundSnake.src = "img/background.jpg";
+     ctx.drawImage(groundSnake,0,0);
+    
+     //add the food: the star image
+       const starImg = new Image();
+       starImg.src = "img/star.jpg";
+      ctx.drawImage(starImg, star.x, star.y)
+    
+
+    //draw the snake
+    for (let i=0;i<snake.length;i++){
+    ctx.fillStyle = "#FF0000"
+    ctx.fillRect(snake[i].x, snake[i].y, unity, unity)}
+   /*make your snake move*/
+   //first create a new head depend on wich case is the direction
+   let snakeNewheadX=snake[0].x
+   let snakeNewheadY=snake[0].y
+   
+   switch(direction) {
+    case "LEFT":
+        snakeNewheadX -= unity
+         break;
+    case "RIGHT":
+        snakeNewheadX += unity
+         break;
+    case "UP":
+        snakeNewheadY -= unity;
+         break;
+    case "DOWN":
+        snakeNewheadY += unity;
+         break;
+
+   }
+    
+  //add the new head to the snake
+  
+  if(snakeNewheadX==star.x&&snakeNewheadY==star.y){
+      //addscore
+      score++
+      //addsound
+      //generete new food = 
+    }
+  
+  else{
+  snake.pop()}
+  
+let newHead = {
+        x : snakeNewheadX,
+        y : snakeNewheadY
+    }
+snake.unshift(newHead)
+   /*when the game over*/
+   
+
+  //add the text score
+  ctx.fillStyle = "black";
+  
+  ctx.fillText(score,unity,unity)
+
+
+}
+
+let justPlay = setInterval(draw,100);
