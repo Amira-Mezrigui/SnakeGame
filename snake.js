8f6341c69c427ec12a67e24b3b8ef42fb69944ac
Amira-Mezrigui <amira.mezrigui@etudiant-fst.utm.tn>
2020-04-16 15:15:05 +0100

    sound effect, a little bit responsive , bombe and star and gameover

--- snake.js
+++ snake.js
@@ -1,33 +1,28 @@
-/*add variables that i need*/
+
 //add the canvas
 var canvas = document.getElementById("snakeCanvas");
 var ctx = canvas.getContext("2d");
 
-// the unity of background
+// the unity of canvas
 const unity = 20;
-
-
 // create the snake
 
 let snake = [{
     x :  5*unity,
     y :  5*unity
 }];
-//generate the food
-
-console.log(star)
 
 // create the score var
-
 let score = 0;
 
-/*function that allows me to know the directio of the snake*/
+/*function that allows me to know the direction of the snake*/
 
 let direction;
 
 document.addEventListener("keydown",path);
 
 function path(event){
+    
     let key = event.which;
     if (key == 37||key== 81 && direction!= "RIGHT"){
         direction = "LEFT";
@@ -39,44 +34,67 @@ function path(event){
         direction= "DOWN";
     }
 }
-     /*create the game*/
-function draw(){
-    /*draw elements of the game*/
-    //add the background
 
-     const groundSnake = new Image();
-     groundSnake.src = "img/background.jpg";
-     ctx.drawImage(groundSnake,0,0);
-    
-     //add the food: the star image
-       const starImg = new Image();
-       starImg.src = "img/star.jpg";
-      ctx.drawImage(starImg, star.x, star.y)
-    
+//Generate the background
+const background = new Image();
+background.src = "img/background.jpg";
+
+//generate the star and the bombe
+    const starImage = new Image();
+    starImage.src = "img/star.jpg";
+    const bombeImage = new Image();
+    bombeImage.src = "img/bombe.jpg";
+
+    var star = {
+        x : Math.floor(Math.random()*25) *unity,
+        y : Math.floor(Math.random()*20+5) *unity
+    }
+    var bombe = {
+        x : Math.floor(Math.random()*25) *unity,
+        y : Math.floor(Math.random()*20+5) *unity
+    }
+
+
+     /*create the game*/
+
+
+function draw(){
+    //draw the background
+    ctx.drawImage(background,0,0);
 
     //draw the snake
     for (let i=0;i<snake.length;i++){
     ctx.fillStyle = "#FF0000"
     ctx.fillRect(snake[i].x, snake[i].y, unity, unity)}
+    // draw the star and bombe
+    ctx.drawImage(starImage, star.x, star.y);
+    ctx.drawImage(bombeImage, bombe.x, bombe.y);
+
    /*make your snake move*/
    //first create a new head depend on wich case is the direction
    let snakeNewheadX=snake[0].x
    let snakeNewheadY=snake[0].y
-   
+// generate move sound
+        let move = new Audio();
+        move.src = "audio/move.mp3";
    switch(direction) {
     case "LEFT":
         snakeNewheadX -= unity
+        move.play();
          break;
     case "RIGHT":
         snakeNewheadX += unity
+        move.play();
          break;
     case "UP":
         snakeNewheadY -= unity;
+        move.play();
+
          break;
     case "DOWN":
         snakeNewheadY += unity;
+        move.play();
          break;
-
    }
     
   //add the new head to the snake
@@ -84,8 +102,15 @@ function draw(){
   if(snakeNewheadX==star.x&&snakeNewheadY==star.y){
       //addscore
       score++
-      //addsound
-      //generete new food = 
+      //addsound when the snake eat food
+      let eat = new Audio();
+          eat.src = "audio/eat.wav";
+          eat.play();
+      //generete new star
+      star = {
+        x : Math.floor(Math.random()*25) * unity,
+        y : Math.floor(Math.random()*20+5) * unity
+    }
     }
   
   else{
@@ -95,16 +120,20 @@ let newHead = {
         x : snakeNewheadX,
         y : snakeNewheadY
     }
-snake.unshift(newHead)
-   /*when the game over*/
-   
 
+   /*when the game over*/
+   if(snakeNewheadX < 0 || snakeNewheadX ==25 * unity || snakeNewheadY < 5*unity || 
+    snakeNewheadY == 25*unity ||  (snakeNewheadX == bombe.x && snakeNewheadY == bombe.y)){
+    clearInterval(justPlay);
+    // add sound of dead
+    let dead = new Audio();
+        dead.src = "audio/dead.mp3";
+        dead.play();
+}
+snake.unshift(newHead)
   //add the text score
   ctx.fillStyle = "black";
-  
-  ctx.fillText(score,unity,unity)
-
-
+  ctx.fillText(score,24*unity,3*unity)
 }
 
-let justPlay = setInterval(draw,100);
+var justPlay = setInterval(draw,100);
